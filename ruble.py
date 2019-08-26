51fbe692d4741ac2818a103d13e32344f18eb015
alex <alex-just2200@yandex.ru>
2019-08-26 16:37:50 +0300

    доработка для работы под терминал

--- ruble.py
+++ ruble.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
 import requests
 from bs4 import BeautifulSoup
+import re
+import os
+import sys
 
 
 def get_html():
@@ -7,13 +11,39 @@ def get_html():
     r = requests.get(url)
     return r.text
 
-def get_rate(html):
-    soup = BeautifulSoup(html, 'lxml')
-    t = soup.find('td', text='USD').find_parent().find_all('td')[-1].text
-    print(t)
+def get_rate(currency):
+    """Находит курс выбранной валюты. По умолчанию - USD"""
+    currency = currency.upper()
+    soup = BeautifulSoup(get_html(), 'lxml')
+    result = soup.find('td', text=currency).find_parent().find_all('td')[-1].text
+    return result
 
-def main():
-    get_rate(get_html())
+def cur_all():
+    'Выводит список всех возможных валют'
+    soup = BeautifulSoup(get_html(), 'lxml')
+    abbreviation = soup.find_all('td', text=re.compile('[A-Z]'))
+    currencies = soup.find_all('td', text=re.compile('[А-я]'))
+    n = 0
+    for abr in abbreviation:
+        print(abr.text + '-' + currencies[n].text)
+        n += 1
+
+def send_message(message):
+    os.system('echo "{}"'.format(message))
+
+def main(currency='USD'):
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'all':
+            cur_all()
+        currency = sys.argv[1]
+
+    try:
+        rate = get_rate(currency)
+        send_message(rate)
+    except AttributeError:
+        rate = ('Не корректная абривеатура валюты. Для вывода всех возможных воспользуйтесь "all" ')
+        send_message(rate)
+
 
 
 if __name__ == '__main__':
