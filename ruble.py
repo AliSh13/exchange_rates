9a73270a663dd08f916574af9b26fb92dcc73de8
alex <alex-just2200@yandex.ru>
2019-08-29 16:58:26 +0300

    рефакторинг

--- ruble.py
+++ ruble.py
@@ -6,25 +6,27 @@ import os
 import sys
 
 class GetRates():
-    def __init__(self, currency='USD'):
+    def __init__(self, currency='USD', units='1'):
         self.currency = currency
+        self.units = units
 
     def get_html(self):
         url = 'https://www.cbr.ru/currency_base/daily/'
         r = requests.get(url)
         return r.text
 
-    def get_rate(self, currency):
-        """Находит курс выбранной валюты. По умолчанию - USD"""
+    def get_rate(self, currency, units):
+        """Находит курс выбранной валюты(по умолчанию - USD) и приводит к читаемому виду. """
         currency = currency.upper()
         soup = BeautifulSoup(self.get_html(), 'lxml')
         result = soup.find('td', text=currency).find_parent().find_all('td')[-1].text
-        return result
+        result = float(result.replace(',','.')) * float(units)
+        return "{0:3.2f} руб.".format(result)
 
     def cur_all(self):
         'Выводит список всех возможных валют'
         soup = BeautifulSoup(self.get_html(), 'lxml')
-        abbreviation = soup.find_all('td', text=re.compile('[A-Z]'))
+        abbreviation = soup.find_all('td', text=re.compile('[A-Z]{3}'))
         currencies = soup.find_all('td', text=re.compile('[А-я]'))
         n = 0
         for abr in abbreviation:
@@ -36,18 +38,25 @@ class GetRates():
         os.system('echo "{}"'.format(message))
 
     def run(self):
-        """ Запускает скрипт, предварительно проверив наличие аргументов"""
-        if len(sys.argv) > 1:
-            if sys.argv[1].upper() == 'ALL':
-                return self.cur_all()
-            self.currency = sys.argv[1]
-
-        """if self.currency.upper() == 'ALL':
-            return self.cur_all()"""
-
+        """ Запускает скрипт, предварительно проверив наличие аргументов и их допустимость"""
         try:
-            rate = self.get_rate(self.currency)
-            self.send_message(rate)
+            arg = sys.argv
+            if len(arg) == 2:
+                self.currency = arg[1].upper()
+            elif len(arg) == 3:
+                self.currency = arg[1].upper()
+                self.units = arg[2]
+
+
+            if self.currency.upper() == 'ALL':
+                return self.cur_all()
+                """проверяем правильность аргументов"""
+            if self.currency.isalpha() and self.units.isdigit():
+                rate = self.get_rate(self.currency, self.units)
+                self.send_message(rate)
+            else:
+                raise AttributeError
+
         except AttributeError:
             #перехватывает исключение не корректного атрибута.
             messge = ('Не корректная аббривеатура валюты. Для вывода всех возможных воспользуйтесь "all" ')
